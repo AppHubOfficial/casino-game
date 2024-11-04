3803ac47a31b58418d6a358eefde88b01af34276
simone <simone07andreotti@gmail.com>
2024-11-04 12:59:24 +0100

    Correzioni

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
-import Header, { LogoLink, NavLinks, NavLink } from '../component/headers/light';
+import Header from '../component/Header'
 import '../assets/styles/Home.css'
-import '../assets/styles/GlobalStyles.js'
 
 const Home = () => {
-    const links = (
-        <NavLinks>
-            <NavLink href="/#">Home</NavLink>
-            <NavLink href="/#">About</NavLink>
-            <NavLink href="/#">Services</NavLink>
-            <NavLink href="/#">Contact</NavLink>
-        </NavLinks>
-    );
-    return (
-        <>
-            <Header links={links} />
-        </>
-    );
+
+  return (
+    <>
+        <Header></Header>
+    </>
+  );
 };
 
 export default Home;
