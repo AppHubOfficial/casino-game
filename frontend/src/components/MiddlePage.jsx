7d8c7194a8934076be8508821924fe46575c8316
simone <simone07andreotti@gmail.com>
2024-11-05 09:49:06 +0100

    new file:   frontend/src/assets/images/PokerIcon.jsx 	new file:   frontend/src/assets/images/roulette.jpg 	modified:   frontend/src/assets/styles/Header.css 	modified:   frontend/src/assets/styles/MiddlePage.css 	modified:   frontend/src/components/Header.jsx 	modified:   frontend/src/components/MiddlePage.jsx

--- frontend/src/components/MiddlePage.jsx
+++ frontend/src/components/MiddlePage.jsx
@@ -1,60 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Button, Box, Stack, Typography } from '@mui/material';
-import CasinoIcon from '@mui/icons-material/Casino';
-
 import '../assets/styles/MiddlePage.css';
+import PokerIcon from '../assets/images/PokerIcon';
 
-import '../assets/styles/Header.css';
+import RouletteImage from '../assets/images/roulette.jpg'
+
 
 const MiddlePage = () => {
-
   return (
     <Box sx={{ py: 5 }} className="middlePageContainer" id = "middleSection">
       <Typography variant="h3" gutterBottom>
         Welcome to the Ultimate Casino Experience!
       </Typography>
-      <Typography variant="h6" gutterBottom sx={{ maxWidth: '600px', mx: 'auto' }}>
-        Play your favorite games, try your luck, and enjoy the thrill of winning!
+      <Typography variant="h6" gutterBottom style={{ maxWidth: '600px', marginTop: '40px', marginBottom: '50px' }}>
+        Play your favorite games, try your luck, and <span style={{ color: '#FFD700' }}>enjoy the thrill of winning!</span>
       </Typography>
+      <img src={RouletteImage} alt="Roulette" className="rouletteImage" />
+      <PokerIcon></PokerIcon>
 
-      <Stack
-        direction="row"
-        spacing={3}
-        justifyContent="center"
-        sx={{ mt: 4, flexWrap: 'wrap' }}
-      >
-        <Button
-          component={Link}
-          to="/games"
-          variant="contained"
-          color="secondary"
-          startIcon={<CasinoIcon />}
-          sx={{ minWidth: '200px' }}
-        >
-          Start Playing
-        </Button>
-
-        <Button
-          component={Link}
-          to="/promotions"
-          variant="outlined"
-          color="primary"
-          sx={{ minWidth: '200px' }}
-        >
-          Check Promotions
-        </Button>
-
-        <Button
-          component={Link}
-          to="/vip"
-          variant="outlined"
-          color="primary"
-          sx={{ minWidth: '200px' }}
-        >
-          Join VIP Club
-        </Button>
-      </Stack>
     </Box>
   );
 };
